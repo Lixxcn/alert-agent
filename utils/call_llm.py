d8ea51a0f8a437ce9286f7c82583242ca9e48873
Lixx <i@lixx.cn>
2025-05-23 17:36:36 +0800

    添加 OpenAI 配置和更新调用 LLM 的功能

--- utils/call_llm.py
+++ utils/call_llm.py
@@ -1,15 +1,19 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Learn more about calling the LLM: https://the-pocket.github.io/PocketFlow/utility_function/llm.html
-def call_llm(prompt):    
+def call_llm(prompt):
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "your-api-key"))
     r = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{"role": "user", "content": prompt}]
+        model="deepseek-chat", messages=[{"role": "user", "content": prompt}]
     )
     return r.choices[0].message.content
-    
+
+
 if __name__ == "__main__":
-    prompt = "What is the meaning of life?"
+    prompt = "什么是LLM?"
     print(call_llm(prompt))
